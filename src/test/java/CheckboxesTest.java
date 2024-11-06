ee133b80c43be125adc974e47aa363376750a98c
alittlemayhem <parabola657@gmail.com>
2024-11-06 12:35:10 +0300

    added solutions to hometask 17

---
+++ src/test/java/CheckboxesTest.java
@@ -0,0 +1,50 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+import java.util.List;
+
+public class CheckboxesTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        // Почему через ChromeOptions, а не просто через .maximize()
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void checkCheckboxes() {
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+
+        SoftAssert softAssert = new SoftAssert();
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("[type=checkbox]"));
+        softAssert.assertFalse(checkboxes.get(0).isSelected());
+
+        checkboxes.get(0).click();
+        softAssert.assertTrue(checkboxes.get(0).isSelected());
+
+        softAssert.assertTrue(checkboxes.get(1).isSelected());
+
+        checkboxes.get(1).click();
+        softAssert.assertFalse(checkboxes.get(1).isSelected());
+        softAssert.assertAll();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/TyposTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TyposTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void checkTypos() {
+        driver.get("http://the-internet.herokuapp.com/typos");
+        String text = driver.findElement(By.tagName("p")).getText();
+
+        String correctText = "won't";
+
+        Assert.assertFalse(text.contains(correctText), "There's a typo!");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
